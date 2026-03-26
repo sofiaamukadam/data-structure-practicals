c4358ff5d5ccd74417970440a80d39a2ca3fdeb9
sofia mukadam <sofiaamukadam@gmail.com>
2026-03-26 13:12:08 +0530

    Update linear search.c

--- linear search.c
+++ linear search.c
@@ -1,17 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-	int a[5],element,i;
-	printf("enter element for array");
+	int a[5],i,element;
+	
 	for(i=0;i<5;i++){
-	scanf("%d",&a[i]);}
-	 printf("enter the searching element");
+		printf("enter element for a[%d]",i);
+		scanf("%d",&a[i]);
+	}
+	 printf("Enter searching element:");
 	 scanf("%d",&element);
-	 for (int i=0;i<5;i++)
-	 {
-	 	if(a[i]==element)
-		{
-	 		printf("element is %d",i);
-		 }
-    }
-    return 0;
-            }
+	 
+	 for(i=0;i<5;i++){
+	 	if (a[i]==element){
+		 
+	 	printf("element is in a[%d]",i);
+	 }
+}
+	 return 0;
+	 
+}
